d4c23ef5516054945a12f665d28845bb7a4b2e49
Mostafa Ayman <mostafa.aymanae@gmail.com>
2021-10-14 12:23:50 +0200

    change the color of the car when picking packages

--- Delivery Driver/Assets/Scripts/Delivery.cs
+++ Delivery Driver/Assets/Scripts/Delivery.cs
@@ -7,6 +7,15 @@ public class Delivery : MonoBehaviour
     bool hasPackage = false;
     [SerializeField]
     float pickupDelay = 0.3f;
+
+    [SerializeField] Color hasPackageColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+    [SerializeField] Color noPackageColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
+    SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Ouch!");
@@ -19,11 +28,13 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package collected");
             hasPackage = true;
             Destroy(collision.gameObject, pickupDelay);
+            spriteRenderer.color = hasPackageColor;
         }
         if (collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package deliverd");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
--- Delivery Driver/Assets/Scripts/Driver.cs
+++ Delivery Driver/Assets/Scripts/Driver.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+
     [SerializeField]
     float steerSpeed = 270f;
     [SerializeField]
     float moveSpeed = 20f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
